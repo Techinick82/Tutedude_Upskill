6958caf3e2a72525fef4d0d57c35ecb0d65d37d3
Techinick82 <116661366+Techinick82@users.noreply.github.com>
2025-07-10 16:11:36 +0530

    Add files via upload

---
+++ Task#1. Even_Odd.py
@@ -0,0 +1,7 @@
+#Testing if the number is Even or Odd
+a=int(input('Enter the number of your choice: '))
+#print('The number you entered is: ',a)
+if a%2 == 0:
+    print(a,'is an even number')
+else:
+    print(a,'is an odd number')
